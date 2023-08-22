0069ad3c79621e502731a469479a37bb8b899072
Johan Rossouw <johan@electric-shed.com.au>
2023-08-22 08:40:24 +0800

    Semi working

--- drivers/z-uno/device.js
+++ drivers/z-uno/device.js
@@ -4,7 +4,7 @@ const { ZwaveDevice } = require('homey-zwavedriver');
 
 class MyDevice extends ZwaveDevice {
 
-  async onNodeInit() {
+  async onMeshInit() {
     // enable debugging
     this.enableDebug();
 
@@ -62,49 +62,14 @@ class MyDevice extends ZwaveDevice {
   }
 
   /**
-   * onInit is called when the device is initialized.
-   */
+    * onInit is called when the device is initialized.
+    */
   async onInit() {
     this.registerCapabilityListener('onoff', async (value) => {
       await this.setCapabilityValue({ on: value });
       this._setCapabilityValue('onoff', 'SWITCH_BINARY', value);
     });
     this.log('MyDevice has been initialized');
-  }
-
-  /**
-   * onAdded is called when the user adds the device, called just after pairing.
-   */
-  async onAdded() {
-    this.log('MyDevice has been added');
-  }
-
-  /**
-   * onSettings is called when the user updates the device's settings.
-   * @param {object} event the onSettings event data
-   * @param {object} event.oldSettings The old settings object
-   * @param {object} event.newSettings The new settings object
-   * @param {string[]} event.changedKeys An array of keys changed since the previous version
-   * @returns {Promise<string|void>} return a custom message that will be displayed
-   */
-  async onSettings({ oldSettings, newSettings, changedKeys }) {
-    this.log('MyDevice settings where changed');
-  }
-
-  /**
-   * onRenamed is called when the user updates the device's name.
-   * This method can be used this to synchronise the name to the device.
-   * @param {string} name The new name
-   */
-  async onRenamed(name) {
-    this.log('MyDevice was renamed');
-  }
-
-  /**
-   * onDeleted is called when the user deleted the device.
-   */
-  async onDeleted() {
-    this.log('MyDevice has been deleted');
   }
 
 }
